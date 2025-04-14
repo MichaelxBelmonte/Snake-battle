41d38e7911b4131f550943fce94ffd0b3d7f26ca
Mike|lasagna <148987376+MichaelxBelmonte@users.noreply.github.com>
2025-04-14 15:18:41 +0200

    Fix: Risolti errori con moduli di test nel webpack config e colore del serpente

--- next.config.js
+++ next.config.js
@@ -7,5 +7,14 @@ module.exports = {
         destination: 'https://snake-battle.vercel.app/api/:path*'
       }
     ]
+  },
+  webpack: (config, { isServer }) => {
+    // Escludi i file di benchmark e test
+    config.module.rules.push({
+      test: /node_modules\/.*\/(test|tests|benchmark|benchmarks|bench)\/.*/,
+      use: 'null-loader',
+    });
+    
+    return config;
   }
 } 
